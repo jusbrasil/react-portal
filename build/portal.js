55aeea398975d2f6487da7b4d14e69933f5a41f4
Andrews Medina <andrewsmedina@gmail.com>
2017-10-06 15:25:45 -0300

    dont use shallowCompare andd CSSPropertyOperations
    https://github.com/tajo/react-portal/pull/105/files

--- build/portal.js
+++ build/portal.js
@@ -4,25 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _index = require('/Users/andrews/projects/jus/react-portal/node_modules/redbox-react/lib/index.js');
-
-var _index2 = _interopRequireDefault(_index);
-
-var _index3 = require('/Users/andrews/projects/jus/react-portal/node_modules/react-transform-catch-errors/lib/index.js');
-
-var _index4 = _interopRequireDefault(_index3);
-
-var _react2 = require('react');
-
-var _react3 = _interopRequireDefault(_react2);
-
-var _index5 = require('/Users/andrews/projects/jus/react-portal/node_modules/react-transform-hmr/lib/index.js');
-
-var _index6 = _interopRequireDefault(_index5);
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _class, _temp;
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
 
 var _propTypes = require('prop-types');
 
@@ -32,14 +18,6 @@ var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _CSSPropertyOperations = require('react-dom/lib/CSSPropertyOperations');
-
-var _CSSPropertyOperations2 = _interopRequireDefault(_CSSPropertyOperations);
-
-var _shallowCompare = require('react/lib/shallowCompare');
-
-var _shallowCompare2 = _interopRequireDefault(_shallowCompare);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -48,38 +26,12 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _components = {
-  Portal: {
-    displayName: 'Portal'
-  }
-};
-
-var _UsersAndrewsProjectsJusReactPortalNode_modulesReactTransformHmrLibIndexJs2 = (0, _index6.default)({
-  filename: './lib/portal.js',
-  components: _components,
-  locals: [module],
-  imports: [_react3.default]
-});
-
-var _UsersAndrewsProjectsJusReactPortalNode_modulesReactTransformCatchErrorsLibIndexJs2 = (0, _index4.default)({
-  filename: './lib/portal.js',
-  components: _components,
-  locals: [],
-  imports: [_react3.default, _index2.default]
-});
-
-function _wrapComponent(id) {
-  return function (Component) {
-    return _UsersAndrewsProjectsJusReactPortalNode_modulesReactTransformHmrLibIndexJs2(_UsersAndrewsProjectsJusReactPortalNode_modulesReactTransformCatchErrorsLibIndexJs2(Component, id), id);
-  };
-}
-
 var KEYCODES = {
   ESCAPE: 27
 };
 
-var Portal = _wrapComponent('Portal')((_temp = _class = function (_React$Component) {
-  _inherits(Portal, _React$Component);
+var Portal = function (_React$PureComponent) {
+  _inherits(Portal, _React$PureComponent);
 
   function Portal() {
     _classCallCheck(this, Portal);
@@ -133,11 +85,6 @@ var Portal = _wrapComponent('Portal')((_temp = _class = function (_React$Compone
       if (typeof newProps.isOpened === 'undefined' && this.state.active) {
         this.renderPortal(newProps);
       }
-    }
-  }, {
-    key: 'shouldComponentUpdate',
-    value: function shouldComponentUpdate(nextProps, nextState) {
-      return (0, _shallowCompare2.default)(this, nextProps, nextState);
     }
   }, {
     key: 'componentWillUnmount',
@@ -225,19 +172,6 @@ var Portal = _wrapComponent('Portal')((_temp = _class = function (_React$Compone
       }
     }
   }, {
-    key: 'applyClassNameAndStyle',
-    value: function applyClassNameAndStyle(props) {
-      if (props.className) {
-        this.node.className = props.className;
-      }
-      if (props.style) {
-        // React 15.1.0+ requires third parameter in debug mode
-        /* eslint-disable no-underscore-dangle */
-        _CSSPropertyOperations2.default.setValueForStyles(this.node, props.style, this._reactInternalInstance);
-        /* eslint-enable no-underscore-dangle */
-      }
-    }
-  }, {
     key: 'targetElement',
     value: function targetElement(targetSelector) {
       var htmlElement = document.querySelector(targetSelector);
@@ -251,19 +185,14 @@ var Portal = _wrapComponent('Portal')((_temp = _class = function (_React$Compone
     value: function renderPortal(props) {
       if (!this.node) {
         this.node = document.createElement('div');
-        // apply CSS before the node is added to the DOM to avoid needless reflows
-        this.applyClassNameAndStyle(props);
         var htmlElement = this.targetElement(props.targetSelector);
         htmlElement.appendChild(this.node);
-      } else {
-        // update CSS when new props arrive
-        this.applyClassNameAndStyle(props);
       }
 
       var children = props.children;
       // https://gist.github.com/jimfb/d99e0678e9da715ccf6454961ef04d1b
       if (typeof props.children.type === 'function') {
-        children = _react3.default.cloneElement(props.children, { closePortal: this.closePortal });
+        children = _react2.default.cloneElement(props.children, { closePortal: this.closePortal });
       }
 
       this.portal = _reactDom2.default.unstable_renderSubtreeIntoContainer(this, children, this.node, this.props.onUpdate);
@@ -272,21 +201,22 @@ var Portal = _wrapComponent('Portal')((_temp = _class = function (_React$Compone
     key: 'render',
     value: function render() {
       if (this.props.openByClickOn) {
-        return _react3.default.cloneElement(this.props.openByClickOn, { onClick: this.handleWrapperClick });
+        return _react2.default.cloneElement(this.props.openByClickOn, { onClick: this.handleWrapperClick });
       }
       return null;
     }
   }]);
 
   return Portal;
-}(_react3.default.Component), _class.defaultProps = {
+}(_react2.default.PureComponent);
+
+Portal.defaultProps = {
   targetSelector: 'body',
   onOpen: function onOpen() {},
   onClose: function onClose() {},
   onUpdate: function onUpdate() {}
-}, _class.propTypes = {
-  className: _propTypes2.default.string,
-  style: _propTypes2.default.object,
+};
+Portal.propTypes = {
   children: _propTypes2.default.element.isRequired,
   targetSelector: _propTypes2.default.string,
   openByClickOn: _propTypes2.default.element,
@@ -297,7 +227,6 @@ var Portal = _wrapComponent('Portal')((_temp = _class = function (_React$Compone
   onClose: _propTypes2.default.func,
   beforeClose: _propTypes2.default.func,
   onUpdate: _propTypes2.default.func
-}, _temp));
-
+};
 exports.default = Portal;
 module.exports = exports['default'];
